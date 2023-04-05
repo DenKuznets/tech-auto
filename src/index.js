8a26616220d75ad074e2fa628142982ad332ebb7
Denis Kuznetsov workPC <denis.kyznecov@gmail.com>
2023-04-05 13:49:11 +0300

    services done

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-import './index.html';
-import './index.scss';
+import "./index.html";
+import "./index.scss";
+
+// const cards = document.getElementsByClassName("services__card");
+// const btn = document.createElement('button');
+// btn.innerText = 'ОСТАВИТЬ ЗАЯВКУ';
+// btn.classList.add("services__card__btn-cta");
+// for (let card of cards) {
+//   // console.log(card);
+//   card.addEventListener("mouseover", function (e) {
+//     card.classList.add("services__card-active");
+//     card.appendChild(btn);
+//   });
+//   card.addEventListener("mouseout", function (e) {
+//     console.log(e.target);
+//     card.classList.remove("services__card-active");
+//     card.removeChild(btn);
+//   });
+// }
 
 
-// import code from './img/code.png'
-// import { mult, sum } from './modules/calc';
-//
-// const imgWrap = document.querySelector('.img');
-// const img = new Image();
-// img.src = code;
-// img.width = 700;
-// imgWrap.append(img);
-//
-// console.log(mult(3, 4));
-// console.log(sum(3, 4));
