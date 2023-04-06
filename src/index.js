500ec049b26acc6d7d06a48afcd7be8025f93a48
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-04-06 17:38:15 +0300

    close popup on click away

--- src/index.js
+++ src/index.js
@@ -2,21 +2,39 @@ import "./index.html";
 import "./index.scss";
 import IMask from "imask";
 
+// нажатие кнопки Оставить заявку на главной
 const heroBtn = document.getElementById("hero-btn");
 heroBtn.addEventListener('click', function () {
   document.getElementById('popup').classList.add('popup-show')
   document.body.classList.add('modal-open')
 })
 
+// нажатие кнопки закрытия формы попапа
 const popupBtn = document.getElementById("popup__close-btn");
-
 popupBtn.addEventListener("click", function () {
-  document.getElementById("popup").classList.remove("popup-show");
-  document.body.classList.remove("modal-open");
+  closePopup();
 });
 
+
+// закрытие попапа по клику мимо
+document.getElementById('popup').addEventListener('click', function (e) {
+  if (e.target.classList.contains("popup")) {
+    closePopup();
+  }
+})
+
+// закрыть попап
+function closePopup() {
+  document.getElementById("popup").classList.remove("popup-show");
+  document.body.classList.remove("modal-open");
+}
+
+// маска для ввода телефона
 let element = document.querySelector('.form-phone');
 let maskOptions = {
   mask: "+{7}(000)000-00-00",
 };
 let mask = IMask(element, maskOptions);
+
+
+
