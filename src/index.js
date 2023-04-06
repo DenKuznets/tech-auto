040bdd8d70345d820bed4cba67b11aa362fd79fc
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-04-06 17:28:57 +0300

    phone mask added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./index.html";
 import "./index.scss";
+import IMask from "imask";
 
 const heroBtn = document.getElementById("hero-btn");
 heroBtn.addEventListener('click', function () {
@@ -13,3 +14,9 @@ popupBtn.addEventListener("click", function () {
   document.getElementById("popup").classList.remove("popup-show");
   document.body.classList.remove("modal-open");
 });
+
+let element = document.querySelector('.form-phone');
+let maskOptions = {
+  mask: "+{7}(000)000-00-00",
+};
+let mask = IMask(element, maskOptions);
