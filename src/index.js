69b1a2fe70b007e5fbce02183129e183bbcc5dbc
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-04-06 17:17:19 +0300

    button listeners

--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
 import "./index.html";
 import "./index.scss";
 
-// const cards = document.getElementsByClassName("services__card");
-// const btn = document.createElement('button');
-// btn.innerText = 'ОСТАВИТЬ ЗАЯВКУ';
-// btn.classList.add("services__card__btn-cta");
-// for (let card of cards) {
-//   // console.log(card);
-//   card.addEventListener("mouseover", function (e) {
-//     card.classList.add("services__card-active");
-//     card.appendChild(btn);
-//   });
-//   card.addEventListener("mouseout", function (e) {
-//     console.log(e.target);
-//     card.classList.remove("services__card-active");
-//     card.removeChild(btn);
-//   });
-// }
+const heroBtn = document.getElementById("hero-btn");
+heroBtn.addEventListener('click', function () {
+  document.getElementById('popup').classList.add('popup-show')
+  document.body.classList.add('modal-open')
+})
 
+const popupBtn = document.getElementById("popup__close-btn");
 
+popupBtn.addEventListener("click", function () {
+  document.getElementById("popup").classList.remove("popup-show");
+  document.body.classList.remove("modal-open");
+});
