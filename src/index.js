0477000b27aef1f1139cdae201d0159accd18823
denis kuznetsov <denis.kyznecov@gmail.com>
2023-04-07 11:49:39 +0300

    dots refactored, ymaps added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import "./index.html";
 import "./index.scss";
 import IMask from "imask";
+import ymaps from "ymaps";
 
 // нажатие кнопки Оставить заявку на главной
 const heroBtn = document.getElementById("hero-btn");
-heroBtn.addEventListener('click', function () {
-  document.getElementById('popup').classList.add('popup-show')
-  document.body.classList.add('modal-open')
-})
+heroBtn.addEventListener("click", function () {
+  document.getElementById("popup").classList.add("popup-show");
+  document.body.classList.add("modal-open");
+});
 
 // закрытие попапа по кнопке
 const popupBtn = document.getElementById("popup__close-btn");
@@ -15,13 +16,12 @@ popupBtn.addEventListener("click", function () {
   closePopup();
 });
 
-
 // закрытие попапа по клику мимо
-document.getElementById('popup').addEventListener('click', function (e) {
+document.getElementById("popup").addEventListener("click", function (e) {
   if (e.target.classList.contains("popup")) {
     closePopup();
   }
-})
+});
 
 // закрыть попап
 function closePopup() {
@@ -30,14 +30,53 @@ function closePopup() {
 }
 
 // маска для ввода телефона
-let element = document.querySelector('.form-phone');
+let element = document.querySelector(".form-phone");
 let maskOptions = {
   mask: "+{7}(000)000-00-00",
 };
 let mask = IMask(element, maskOptions);
 
 // бурег нажатие
-const burger = document.querySelector('.burger');
+const burger = document.querySelector(".burger");
 
-
-
+// YANDEX MAPS
+(() => {
+  ymaps
+    .load("https://api-maps.yandex.ru/2.1/?lang=ru_RU")
+    .then((maps) => {
+      const map = new maps.Map(
+        "map",
+        {
+          // 51.367258, 42.078642
+          center: [51.367258, 42.078642],
+          zoom: 16,
+        },
+        { yandexMapDisablePoiInteractivity: true },
+        {
+          searchControlProvider: "yandex#search",
+        }
+      );
+      let positions = map.getGlobalPixelCenter();
+      //            positions = [
+      //                positions[0] + 420,
+      //                positions[1]
+      //            ]
+      const offsetPos = map.options
+        .get("projection")
+        .fromGlobalPixels(positions, map.getZoom());
+      const myPlacemark = new maps.Placemark(offsetPos, {
+        iconLayout: "default#image",
+        iconImageSize: [50, 50],
+        iconImageOffset: [-8, -72],
+      });
+      // const placemark = new maps.Placemark( map.getCenter(),
+      //     {
+      //         iconLayout: 'default#image',
+      //         iconImageSize: [ 50, 50 ],
+      //         iconImageOffset: [ -8, -72 ]
+      //     }
+      // );
+      map.geoObjects.add(myPlacemark);
+    })
+    .catch((error) => console.log("Failed to load Yandex Maps", error));
+})();
