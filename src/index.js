b052c72c1613eeb870c6c638e2d7fa37871f07bc
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-04-08 12:06:07 +0300

    mobile menu refactoring

--- src/index.js
+++ src/index.js
@@ -122,31 +122,30 @@ const yandexMap = document.getElementById("ymaps");
 // бургер нажатие
 const burger = document.querySelector(".burger");
 const closeBtn = document.querySelector(".mobile-menu__close-btn");
-burger.addEventListener("click", openMobileMenu);
+burger.addEventListener("click", function () {
+  const nav = document.querySelector(".nav__list-header");
+  nav.classList.add("mobile-nav-active");
+  burger.classList.remove("burger-show");
+  burger.classList.add("burger-hide");
+  closeBtn.classList.toggle("mobile-menu__close-btn-active");
+});
 
 // кнопка закрыть мобильное меню нажатие
-closeBtn.addEventListener('click', closeMobileMenu)
+closeBtn.addEventListener("click", closeMobileMenu);
 
 // ссылки в мобильном меню нажатие
 let mobileMenuAnchors = document.querySelectorAll(".nav__item-header");
 for (let anchor of mobileMenuAnchors) {
-  anchor.addEventListener('click', closeMobileMenu)
+  anchor.addEventListener("click", closeMobileMenu);
 }
 
-//закрыть мобильное меню
+// закрыть мобильное меню
 function closeMobileMenu() {
   const nav = document.querySelector(".nav__list-header");
   nav.classList.remove("mobile-nav-active");
-  burger.style.display = "block";
-  closeBtn.style.display = "none";
-  // document.body.classList.remove("modal-open");
-}
-
-// открыть мобильное меню
-function openMobileMenu() {
-  const nav = document.querySelector(".nav__list-header");
-  nav.classList.add("mobile-nav-active");
-  burger.style.display = "none";
-  closeBtn.style.display = "block";
-  // document.body.classList.add("modal-open");
+  if (closeBtn.classList.contains("mobile-menu__close-btn-active")) {
+    closeBtn.classList.toggle("mobile-menu__close-btn-active");
+    burger.classList.add("burger-show");
+    burger.classList.remove("burger-hide");
+  }
 }
